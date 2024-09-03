fd1c35aa07a8f5e53c0271f838f3f215824779be
Rajappan043 <rajappaambal@gmail.com>
2024-09-03 15:16:40 +0530

    commit

---
+++ FrontEnd/src/dashboards/Manager.jsx
@@ -0,0 +1,5 @@
+function Manager() {
+  return <div className="bg-cyan-100 h-screen text-center">Manager</div>;
+}
+
+export default Manager;
---
+++ FrontEnd/src/dashboards/Supervisor.jsx
@@ -0,0 +1,5 @@
+function Supervisor() {
+  return <div className="bg-slate-500 text-center">Supervisor</div>;
+}
+
+export default Supervisor;
